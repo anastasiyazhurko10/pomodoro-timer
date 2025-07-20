ef6001579aafda1927fa732f554f0628b89cd589
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-07-20 19:33:46 +0300

    Написала функцию для отображения отсчета времени
    1. Создала переменную minutes. В ней Math.floor(time / 60) - целое количество минут
    2. Создала переменную seconds. В ней time % 60 - остаток секунд.
    3. Создала переменную formattedSeconds, в ней будут секунды с нулем, если их меньше 10. Использовала условный оператор if, чтобы либо добавить 0, если секунд меньше 10, либо нет.
    4. К переменной display добавила textContent. Здесь собираются минуты и значение formattedSeconds. Если оно меньше 10, тогда будет с нулем, если нет, то, как есть.

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const startBtn = document.getElementById('start');
 const resetBtn = document.getElementById('reset');
 const pomodoroBtn = document.getElementById('pomodoro');
 const breakBtn = document.getElementById('break');
+
+function makeTimeLower() {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    let formattedSeconds;
+
+    if (seconds < 10) {
+        formattedSeconds = '0' + seconds;
+    } else {
+        formattedSeconds = seconds
+    };
+
+    display.textContent = minutes + ':' + formattedSeconds;
+}
